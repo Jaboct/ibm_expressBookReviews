f6f169d87c02caac3a78f2f9ad350c9e98766e0e
Jaboct <jaboct@jaboct.com>
2023-01-04 18:50:51 -0500

    jabocts 3rd commit.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,7 +5,7 @@ const regd_users = express.Router();
 
 let users = [];
 
-//const JWT_SECRET = "aSecretString";
+const JWT_SECRET = "aSecretString";
 
 const isValid = (username)=>{ //returns boolean
     // write code to check is the username is valid
@@ -18,51 +18,79 @@ const isValid = (username)=>{ //returns boolean
     return 1;
 }
 
+/*
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+    //write code to check if username and password match the one we have in records.
+    if ( !username ||
+        username === "" ) {
+       //return res.status(400).json({message: "Username is empty"});
+       return 4;
+   }
+   if ( !password ||
+    password === "" ) {
+       //return res.status(400).json({message: "Password is empty"});
+       return 3;
+   }
+
+   for ( i in users ) {
+       var user = users[i];
+       if ( user.name == username && 
+            user.pass == password ) {
+           // both were successfully matched.
+           return 2;
+       } else {
+//           return res.status(400).json({message: "Username or password failed."});
+            return 1;
+       }
+   }
+//   return res.status(400).end( "User or Password did not match" );
+   return 0;
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
     console.log ( "post /login" );
-//    console.log ( "req: ", req );
-//    console.log ( "req.secret: ", req.secret );
-    console.log ( "req.session: ", req.session );
-    console.log ( "req.session.secret: ", req.session.secret );
-    console.log ( "session.secret: ", session.secret );
-  //Write your code here
-//  return res.status(300).json({message: "Yet to be implemented"});
     var name = req.body.username;
     var pass = req.body.password;
-    if ( !name ||
-         name === "" ) {
+    var ret = authenticatedUser ( name, pass );
+    if ( ret == 0 ) {
+        return res.status(400).end( "User or Password did not match" );        
+    } else if ( ret == 1 ) {
+        return res.status(400).json({message: "Username or password failed."});
+    } else if ( ret == 2 ) {
+        var token = jwt.sign({name: name}, JWT_SECRET);
+        user.token = token;
+        return res.json({
+            token: token,
+        });
+    } else if ( ret == 3 ) {
+        return res.status(400).json({message: "Password is empty"});
+    } else if ( ret == 4 ) {
         return res.status(400).json({message: "Username is empty"});
     }
-    if ( !pass ||
-         pass === "" ) {
-        return res.status(400).json({message: "Password is empty"});
-    }
-
-    for ( i in users ) {
-        var user = users[i];
-        if ( user.name == name && 
-             user.pass == pass ) {
-            // both were successfully matched.
-            var token = jwt.sign({name: name}, JWT_SECRET);
-            user.token = token;
-            return res.json({
-                token: token,
-            });
-        } else {
-            return res.status(400).json({message: "Username or password failed."});
-        }
-    }
 });
+*/
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+//  return res.status(300).json({message: "Yet to be implemented"});
+/*
+    var isbn = req.params.isbn;
+    for ( i in books ) {
+        var book = books[i];
+        if ( book.isbn == isbn ) {
+            var book = books[i];
+//            return res.status(200).end(JSON.stringify(book));
+            // look through this books reviews, if 
+            for ( r in book.reviews ) {
+                console.log ( book.reviews[i] );
+                if ( book.reviews.user == req.)
+            }
+        }
+    }
+*/
+    return res.status(400).end("ISBN could not be found.");
 });
 
 module.exports.authenticated = regd_users;
